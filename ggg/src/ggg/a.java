8e114493b36051889dfcdae6920ab6bd2b8da3f7
sita1999 <ksita251k@gmail.com>
2018-06-22 11:40:22 +0530

    char

--- ggg/src/ggg/a.java
+++ ggg/src/ggg/a.java
@@ -3,9 +3,8 @@ package ggg;
 public class a {
 	public static void main(String[] args) {
 		int a=1, b=2;
-		System.out.print(a+b);
-		System.out.print(a-b);
-		System.out.print(a*b);
+		char choice='a';
+		
 	}
 
 }
