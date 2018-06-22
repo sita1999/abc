0696bede566139c11186d18f636d98eeb8184ec1
sita1999 <ksita251k@gmail.com>
2018-06-22 11:58:42 +0530

    scanner

---
+++ ggg/src/ggg/arr.java
@@ -0,0 +1,12 @@
+package ggg;
+
+import java.util.Scanner;
+
+public class arr  {
+	public static void main(String[] args)
+	{
+		Scanner def=new Scanner(System.in);
+	int num=def.nextInt();
+	System.out.print(num);
+	}
+}
