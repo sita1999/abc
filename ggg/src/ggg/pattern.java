567ab5e4469e65d69dd6bc36d0905bc02eaf776a
sita1999 <ksita251k@gmail.com>
2018-06-23 10:47:21 +0530

    pattern

--- ggg/src/ggg/pattern.java
+++ ggg/src/ggg/pattern.java
@@ -4,7 +4,7 @@ public class pattern {
 public static void main(String[] k) {
 	int i,j;
 	for(i=1;i<=5;i++) {
-		for(j=1;j<i;j++) {
+		for(j=1;j<=i;j++) {
 			System.out.print("*");
 		}
 		System.out.println();
