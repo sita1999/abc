8b66b2e590b81117c5da9631d7c8b5835d28cac2
sita1999 <ksita251k@gmail.com>
2018-06-23 10:46:49 +0530

    pattern

---
+++ ggg/src/ggg/pattern.java
@@ -0,0 +1,13 @@
+package ggg;
+
+public class pattern {
+public static void main(String[] k) {
+	int i,j;
+	for(i=1;i<=5;i++) {
+		for(j=1;j<i;j++) {
+			System.out.print("*");
+		}
+		System.out.println();
+	}
+}
+}
