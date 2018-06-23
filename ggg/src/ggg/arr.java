2733916126658c37cf360c756c502f8a1fdaac50
sita1999 <ksita251k@gmail.com>
2018-06-23 10:01:19 +0530

    two no.

--- ggg/src/ggg/arr.java
+++ ggg/src/ggg/arr.java
@@ -3,10 +3,19 @@ package ggg;
 import java.util.Scanner;
 
 public class arr  {
-	public static void main(String[] args)
-	{
-		Scanner def=new Scanner(System.in);
-	int num=def.nextInt();
-	System.out.print(num);
+	public static void main(String[] args){
+		Scanner sc =new Scanner(System.in);
+		int a,b;
+		a=sc.nextInt();
+		b=sc.nextInt();
+		if(a>b) {
+			if(a==10)
+				System.out.println("equal to 10");
+			else
+				System.out.println(" not equal to 10");
+		}else {
+			System.out.println("less than a");
+		}
 	}
+
 }
