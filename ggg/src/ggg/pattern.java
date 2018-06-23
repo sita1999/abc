335ea0c8b28ec7c88910bfb753bb561ee27ccd7a
sita1999 <ksita251k@gmail.com>
2018-06-23 11:03:19 +0530

    triangle

--- ggg/src/ggg/pattern.java
+++ ggg/src/ggg/pattern.java
@@ -4,6 +4,9 @@ public class pattern {
 public static void main(String[] k) {
 	int i,j;
 	for(i=1;i<=5;i++) {
+		for(j=1;j<=5-i;j++) {
+			System.out.print(" ");
+		}
 		for(j=1;j<=i;j++) {
 			System.out.print("*");
 		}
