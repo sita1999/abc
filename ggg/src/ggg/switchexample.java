ac76b845d6f8980d50a0ff38174f8b280c671031
sita1999 <ksita251k@gmail.com>
2018-06-25 10:33:45 +0530

    oops

--- ggg/src/ggg/switchexample.java
+++ ggg/src/ggg/switchexample.java
@@ -4,24 +4,12 @@ import java.util.Scanner;
 
 public class switchexample {
 	public static void main(String[] args) {
-		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter choice ");
-		int ch=sc.nextInt();
-		switch(ch) {
-		case 1:
-			System.out.println("Choice 1");
-			break;
-		case 2:
-			System.out.println("Choice 2");
-			break;
-		case 3:
-			System.out.println("Choice 3");
-			break;
-		case 4:
-			System.out.println("Choice 4");
-			break;
-			default:
-				System.out.println("Deault");
+		int i,j,k;
+		for(i=1;i<=4;i++) {
+			System.out.print(5);
+		}
+		for(j=1;j<=i;j++) {
+			System.out.println("");
 		}
 	}
 }
